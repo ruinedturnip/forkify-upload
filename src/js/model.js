64c3289e737ae1227bd8467c15a448ea19f5be08
ruinedturnip <evan.sonoranstudios@gmail.com>
2022-12-09 20:50:29 -0500

    url fix search results model state

--- src/js/model.js
+++ src/js/model.js
@@ -33,7 +33,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await AJAX(`${API_URL}/${id}?key=${KEY}`);
+    const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
     state.recipe = createRecipeObject(data);
 
     if (state.bookmarks.some(bookmark => bookmark.id === id)) {
